dafd0fbd33fd9236505704380cadddc6535de560
Petr16 <petruchchio16@gmail.com>
2021-11-04 18:16:14 +0700

    api: [-] Удалил лишние проекты.

--- src/api/CustomerManagerWebApi/CastomerManager.DAL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CastomerManager.DAL
-{
-    public class Class1
-    {
-    }
-}
--- src/api/CustomerManagerWebApi/CustomManager.BLL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CustomManager.BLL
-{
-    public class Class1
-    {
-    }
-}
